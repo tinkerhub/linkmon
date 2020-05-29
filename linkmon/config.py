4e6346343fa115067e398d6d5b81f05aea299ee0
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2020-05-30 01:35:19 +0530

    Added config

---
+++ linkmon/config.py
@@ -0,0 +1,24 @@
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+TOP_LEVEL_DIR = os.path.abspath(os.curdir)
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    TEMPLATES_AUTO_RELOAD = True
+
+class ProductionConfig(Config):
+    DEBUG=False
+
+class StagingConfig(Config):
+    DEVELOPMENT=True
+    DEBUG=True
+
+class StagingConfig(Config):
+    DEVELOPMENT=True
+    DEBUG=True
+
+class TestingConfig(Config):
+    TESTING=True
+    DEBUG=True
