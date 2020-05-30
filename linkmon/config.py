8585d2e5aec117ab2af9ffc006daa202e21bb91e
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2020-05-30 16:26:36 +0530

    Added host

--- linkmon/config.py
+++ linkmon/config.py
@@ -4,6 +4,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 TOP_LEVEL_DIR = os.path.abspath(os.curdir)
 
 class Config(object):
+    HOST = os.environ.get('HOST')
     DEBUG = False
     TESTING = False
     TEMPLATES_AUTO_RELOAD = True
